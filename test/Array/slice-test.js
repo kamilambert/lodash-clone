6fc11e7dd11d466c23c20a0e31f6c3bc732b45c5
Patrick <porchewellness@gmail.com>
2017-04-18 10:27:18 -0700

    fixes the array files after code review

--- test/Array/slice-test.js
+++ test/Array/slice-test.js
@@ -1,10 +1,10 @@
 let expect = require ('chai').expect
-let _slice = require ('../../src/Array/slice')
+let slice = require ('../../src/Array/slice')
 
-describe('_slice()', () => {
+describe('slice()', () => {
   it('Creates a slice of array from start up to but now including end', () => {
-    expect(_slice(['1', '2', '3', '4', '5'], 1, 3)).to.deep.equal(['2','3'])
-    expect(_slice([1, 2, 3, 4, 5], 1, 3)).to.deep.equal([2, 3])
-    expect(_slice([1, '2', [3], 4, 5], 1, 3)).to.deep.equal(['2', [3]])
+    expect(slice(['1', '2', '3', '4', '5'], 1, 3)).to.deep.equal(['2','3'])
+    expect(slice([1, 2, 3, 4, 5], 1, 3)).to.deep.equal([2, 3])
+    expect(slice([1, '2', [3], 4, 5], 1, 3)).to.deep.equal(['2', [3]])
   })
 })
